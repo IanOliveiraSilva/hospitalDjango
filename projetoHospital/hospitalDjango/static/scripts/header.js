904776a32c049f6e35bb599aead86a68e4f00b6d
Larissa-Nahan <larissanahan@gmail.com>
2022-05-25 14:45:39 -0300

    Iniciado menu

--- projetoHospital/hospitalDjango/static/scripts/header.js
+++ projetoHospital/hospitalDjango/static/scripts/header.js
@@ -1,50 +1,57 @@
+const $logo = document.querySelector(".header__title--logo");
+const $hospitalName = document.querySelector(".header__title--title");
+const $menu = document.querySelector(".header__menu");
+const $lines = document.querySelectorAll(".header__menu--line");
+const $menuAlt = document.querySelector(".header__menu--title");
+const $departments = document.querySelector(".menu--departments");
+
 window.onscroll = function () {
-    scrollFunction();
-  };
-  
-  function scrollFunction() {
-    const $lines = document.querySelectorAll(".header__menu--line");
-    
-    if (document.body.scrollTop > 90 || document.documentElement.scrollTop > 90) {
-      const fontSizeTitle = 40 / 16;
-      const fontSizeAlt = 15 / 16;
-  
-      document.querySelector(".header__title--logo").style.width = "40px";
-  
-      document.querySelector(
-        ".header__title--title"
-      ).style.fontSize = `${fontSizeTitle}rem`;
-  
-      document.querySelector(
-        ".header__menu--title"
-      ).style.fontSize = `${fontSizeAlt}rem`;
-  
-      document.querySelector(".header__menu").style.width = "40px";
-      document.querySelector(".header__menu").style.margin = "0";
-  
-      for (let $i = 0; $i < $lines.length; $i++) {
-        $lines[$i].style.height = "2px";
-      }
-    } else {
-      const fontSizeTitle = 56 / 16;
-      const fontSizeAlt = 20 / 16;
-  
-      document.querySelector(".header__title--logo").style.width = "70px";
-  
-      document.querySelector(
-        ".header__title--title"
-      ).style.fontSize = `${fontSizeTitle}rem`;
-  
-      document.querySelector(
-        ".header__menu--title"
-      ).style.fontSize = `${fontSizeAlt}rem`;
-  
-      document.querySelector(".header__menu").style.width = "50px";
-      document.querySelector(".header__menu").style.margin = "2vh 0 0 0";
-  
-      for (let $i = 0; $i < $lines.length; $i++) {
-        $lines[$i].style.height = "4px";
-      }
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 90 || document.documentElement.scrollTop > 90) {
+    const fontSizeTitle = 40 / 16;
+    const fontSizeAlt = 15 / 16;
+
+    $logo.style.width = "40px";
+
+    $hospitalName.style.fontSize = `${fontSizeTitle}rem`;
+
+    $menuAlt.style.fontSize = `${fontSizeAlt}rem`;
+
+    $menu.style.width = "40px";
+    $menu.style.margin = "0";
+    $departments.style.padding = "4% 0 0 0";
+
+    for (let $i = 0; $i < $lines.length; $i++) {
+      $lines[$i].style.height = "2px";
+    }
+  } else {
+    const fontSizeTitle = 56 / 16;
+    const fontSizeAlt = 20 / 16;
+
+    $logo.style.width = "70px";
+
+    $hospitalName.style.fontSize = `${fontSizeTitle}rem`;
+
+    $menuAlt.style.fontSize = `${fontSizeAlt}rem`;
+
+    $menu.style.width = "50px";
+    $menu.style.margin = "2vh 0 0 0";
+    $departments.style.padding = "6% 0 0 0";
+
+    for (let $i = 0; $i < $lines.length; $i++) {
+      $lines[$i].style.height = "4px";
     }
   }
-  
+}
+
+$menu.addEventListener("click", function () {
+  console.log("a");
+  if ($departments.style.display === "none") {
+    $departments.style.display = "block";
+  } else {
+    $departments.style.display = "none";
+  }
+});
