530c140a39b8d8a7df55236bbf547e9dda0c3673
Larissa-Nahan <larissanahan@gmail.com>
2022-05-30 13:31:50 -0300

    Precisa de Ajuda | Quero doar

---
+++ projetoHospital/hospitalDjango/static/scripts/circularMenu.js
@@ -0,0 +1,24 @@
+const $html = document.querySelector("html");
+const $circularMenu = document.querySelector(".side-small-menu");
+const $chatbotMenu = document.querySelector(".chatbot");
+const $doacaoMenu = document.querySelector(".doacao");
+
+// jQuery(document).ready(function ($) {
+//   $(window).scroll(function () {});
+// });
+
+// $(function () {
+//   $(window).scroll(function () {
+//     //Check if it hits the bottom, toggle Relevant Class.
+//     if (window.innerHeight + window.scrollY >= document.body.offsetHeight)
+//       $circularMenu.style.bottom = "30px";
+//     else $circularMenu.style.bottom = "100px";
+//   });
+// });
+console.log("html:" + $html.innerHeight);
+
+// window.onscroll = function(ev) {
+//     if ((window.innerHeight + window.pageYOffset) >= document.body.scrollHeight) {
+//       // you're at the bottom of the page
+//     }
+// };
