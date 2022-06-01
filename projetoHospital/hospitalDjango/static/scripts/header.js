f2d4dfc527538020bb4c9a1c060dfb03fc8c127b
Larissa-Nahan <larissanahan@gmail.com>
2022-06-01 14:00:24 -0300

    arrumando header

--- projetoHospital/hospitalDjango/static/scripts/header.js
+++ projetoHospital/hospitalDjango/static/scripts/header.js
@@ -3,8 +3,6 @@ const $hospitalName = document.querySelector(".header__title--title");
 const $menu = document.querySelector(".header__menu");
 const $lines = document.querySelector(".header__menu--menu");
 const $linesImage = document.querySelector(".header__menu--menu-image");
-const $closeImage = document.querySelector(".header__menu--close-image");
-const $close = document.querySelector(".header__menu--close");
 const $menuAlt = document.querySelector(".header__menu--title");
 const $menuOptions = document.querySelector(".menu-options");
 
@@ -25,11 +23,10 @@ function scrollFunction() {
 
     $hospitalName.style.fontSize = `${fontSizeTitle}rem`;
 
-    $closeImage.style.width = "30px";
     $menuAlt.style.fontSize = `${fontSizeAlt}rem`;
     $menuAlt.style.margin = "40px 0 0 0";
 
-    $menuOptions.style.margin = "-7% 0 0 9.5%";
+    $menuOptions.style.margin = "-8% 0 0 9.5%";
     $menuOptions.style.padding = "5% 0 0 0";
 
     $linesImage.style.width = "40px";
@@ -41,11 +38,10 @@ function scrollFunction() {
 
     $hospitalName.style.fontSize = `${fontSizeTitle}rem`;
 
-    $closeImage.style.width = "40px";
     $menuAlt.style.fontSize = `${fontSizeAlt}rem`;
     $menuAlt.style.margin = "50px 0 0 0";
 
-    $menuOptions.style.margin = "0 0 0 9.5%";
+    $menuOptions.style.margin = "-1% 0 0 9.5%";
     $menuOptions.style.padding = "0 0 0 0";
 
     $linesImage.style.width = "50px";
@@ -53,22 +49,13 @@ function scrollFunction() {
 }
 
 $menu.addEventListener("mouseover", function () {
-  $lines.style.display = "none";
-  $close.style.display = "block";
-  $menuAlt.innerHTML = "Fechar";
   $menuOptions.style.display = "block";
 });
 
 $menuOptions.addEventListener("mouseover", function () {
-  $lines.style.display = "none";
-  $close.style.display = "block";
-  $menuAlt.innerHTML = "Fechar";
   $menuOptions.style.display = "block";
 });
 
 $menuOptions.addEventListener("mouseout", function () {
-  $lines.style.display = "block";
-  $close.style.display = "none";
-  $menuAlt.innerHTML = "Menu";
   $menuOptions.style.display = "none";
 });
