053b2c5506fbd7669469fc2c05698c2f5eebcb8a
Luiz Filipe <luizfilipemr17@gmail.com>
2022-06-03 13:28:43 -0300

    Alterando modelo de data

--- projetoHospital/hospitalDjango/projetoHospital/core/settings.py
+++ projetoHospital/hospitalDjango/projetoHospital/core/settings.py
@@ -126,3 +126,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DATE_FORMAT': "%d/%m/%Y",
+    'DATE_INPUT_FORMATS': ["%d-%m-%Y"],
+}
