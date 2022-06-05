1f1d8140037f0ee3531e76d3d586298dcfac72d4
Candice Brasileiro <candicelopes88@gmail.com>
2022-06-05 12:23:35 -0300

    SM tentativa1JS-header

--- projetoHospital/hospitalDjango/static/scripts/header.js
+++ projetoHospital/hospitalDjango/static/scripts/header.js
@@ -1,4 +1,4 @@
-const $logo = document.querySelector(".header__title--logo");
+const $logo = document.querySelector(".header__title--logo ");
 const $hospitalName = document.querySelector(".header__title--title");
 const $menu = document.querySelector(".header__menu");
 const $lines = document.querySelector(".header__menu--menu");
@@ -60,10 +60,22 @@ $menuOptions.addEventListener("mouseout", function () {
   $menuOptions.style.display = "none";
 });
 
-/* ======================================== M O B I L E     D E V I C E ======================================== 
+
+/*============================ M O B I L E      D E V I C E ============================*/
 
 
-  
+
+const $logoSD = document.querySelector(".header__title--logo-small-device");
+const $hospitalNameSD = document.querySelector(".header__title--title-small-device");
+const $menuSD = document.querySelector(".header__menu-small-device");
+const $linesSD = document.querySelector(".header__menu--menu-small-device");
+const $linesImageSD = document.querySelector(".header__menu--menu-image-small-device");
+const $menuAltSD = document.querySelector(".header__menu--title-small-device");
+const $menuOptionsSD = document.querySelector(".menu-options-small-device");
+
+
+
+
 window.onscroll = function () {
   scrollFunction();
 };
@@ -73,48 +85,35 @@ function sendTo(web) {
 }
 
 function scrollFunction() {
-  if (document.body.scrollTop > 90 || document.documentElement.scrollTop > 90) {
-    const fontSizeTitle = 40 / 16;
-    const fontSizeAlt = 15 / 16;
+  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+    const fontSizeTitleSD = 13.3 / 5.3;
+    const fontSizeAltSD = 5 / 5.3;
 
-    $logo.style.width = "40px";
+    $logoSD.style.width = "13.3px";
 
-    $hospitalName.style.fontSize = `${fontSizeTitle}rem`;
+    $hospitalNameSD.style.fontSize = `${fontSizeTitleSD}rem`;
 
-    $menuAlt.style.fontSize = `${fontSizeAlt}rem`;
-    $menuAlt.style.margin = "40px 0 0 0";
+    $menuAltSD.style.fontSize = `${fontSizeAltSD}rem`;
+    $menuAltSD.style.margin = "13.3px 0 0 0";
 
-    $menuOptions.style.margin = "-8% 0 0 9.5%";
-    $menuOptions.style.padding = "5% 0 0 0";
+    $menuOptionsSD.style.margin = "-2.6% 0 0 3.16%";
+    $menuOptionsSD.style.padding = "1.6% 0 0 0";
 
-    $linesImage.style.width = "40px";
+    $linesImageSD.style.width = "13.3px";
   } else {
-    const fontSizeTitle = 56 / 16;
-    const fontSizeAlt = 20 / 16;
+    const fontSizeTitleSD = 18.6 / 5.3;
+    const fontSizeAltSD = 6.6 / 5.3;
 
-    $logo.style.width = "80px";
+    $logoSD.style.width = "26.6px";
 
-    $hospitalName.style.fontSize = `${fontSizeTitle}rem`;
+    $hospitalNameSD.style.fontSize = `${fontSizeTitleSD}rem`;
 
-    $menuAlt.style.fontSize = `${fontSizeAlt}rem`;
-    $menuAlt.style.margin = "50px 0 0 0";
+    $menuAltSD.style.fontSize = `${fontSizeAltSD}rem`;
+    $menuAltSD.style.margin = "16.6px 0 0 0";
 
-    $menuOptions.style.margin = "-1% 0 0 9.5%";
-    $menuOptions.style.padding = "0 0 0 0";
+    $menuOptionsSD.style.margin = "-0.33% 0 0 3.16%";
+    $menuOptionsSD.style.padding = "0 0 0 0";
 
-    $linesImage.style.width = "50px";
+    $linesImageSD.style.width = "16.6px";
   }
 }
-
-$menu.addEventListener("mouseover", function () {
-  $menuOptions.style.display = "block";
-});
-
-$menuOptions.addEventListener("mouseover", function () {
-  $menuOptions.style.display = "block";
-});
-
-$menuOptions.addEventListener("mouseout", function () {
-  $menuOptions.style.display = "none";
-});
-}*/
