ec1e4066919a7723324dbdb0b2fb62e06640955b
Candice Brasileiro <candicelopes88@gmail.com>
2022-06-04 22:48:49 -0300

    mobile header & js

--- projetoHospital/hospitalDjango/static/scripts/header.js
+++ projetoHospital/hospitalDjango/static/scripts/header.js
@@ -59,3 +59,62 @@ $menuOptions.addEventListener("mouseover", function () {
 $menuOptions.addEventListener("mouseout", function () {
   $menuOptions.style.display = "none";
 });
+
+/* ======================================== M O B I L E     D E V I C E ======================================== 
+
+
+  
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function sendTo(web) {
+  window.location.href = web;
+}
+
+function scrollFunction() {
+  if (document.body.scrollTop > 90 || document.documentElement.scrollTop > 90) {
+    const fontSizeTitle = 40 / 16;
+    const fontSizeAlt = 15 / 16;
+
+    $logo.style.width = "40px";
+
+    $hospitalName.style.fontSize = `${fontSizeTitle}rem`;
+
+    $menuAlt.style.fontSize = `${fontSizeAlt}rem`;
+    $menuAlt.style.margin = "40px 0 0 0";
+
+    $menuOptions.style.margin = "-8% 0 0 9.5%";
+    $menuOptions.style.padding = "5% 0 0 0";
+
+    $linesImage.style.width = "40px";
+  } else {
+    const fontSizeTitle = 56 / 16;
+    const fontSizeAlt = 20 / 16;
+
+    $logo.style.width = "80px";
+
+    $hospitalName.style.fontSize = `${fontSizeTitle}rem`;
+
+    $menuAlt.style.fontSize = `${fontSizeAlt}rem`;
+    $menuAlt.style.margin = "50px 0 0 0";
+
+    $menuOptions.style.margin = "-1% 0 0 9.5%";
+    $menuOptions.style.padding = "0 0 0 0";
+
+    $linesImage.style.width = "50px";
+  }
+}
+
+$menu.addEventListener("mouseover", function () {
+  $menuOptions.style.display = "block";
+});
+
+$menuOptions.addEventListener("mouseover", function () {
+  $menuOptions.style.display = "block";
+});
+
+$menuOptions.addEventListener("mouseout", function () {
+  $menuOptions.style.display = "none";
+});
+}*/
