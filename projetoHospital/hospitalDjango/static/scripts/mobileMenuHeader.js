c3881c06bf45b70c86961493a40f55cffa77895a
Candice Brasileiro <candicelopes88@gmail.com>
2022-06-08 19:31:25 -0300

    menu mobile

--- projetoHospital/hospitalDjango/static/scripts/mobileMenuHeader.js
+++ projetoHospital/hospitalDjango/static/scripts/mobileMenuHeader.js
@@ -1,17 +1,17 @@
 /* @media small devices*/
 const $menuOptions = document.querySelector(".menu-options");
 const $menuDevices = document.querySelector(".header__menu--devices");
-const $menuDevOpt = document.querySelector(".menu-device-options");
 
 
-$menuDevOpt.addEventListener("mouseover", function () {
-  $menuDevOpt.style.display = "block";
+
+$menuOptions.addEventListener("mouseover", function () {
+  $menuOptions.style.display = "block";
 });
 
 $menuDevices.addEventListener("mouseover", function () {
     $menuOptions.style.display = "block";
 });
   
-$menuDevOpt.addEventListener("mouseover", function () {
-  $menuDevOpt.style.display = "none";
+$menuOptions.addEventListener("mouseover", function () {
+  $menuOptions.style.display = "none";
 });
