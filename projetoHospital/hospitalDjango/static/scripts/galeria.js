6a234501d43bfadfd43e82b59760e935f57c8e86
Larissa-Nahan <larissanahan@gmail.com>
2022-06-09 14:51:36 -0300

    Finalizando (espero) galeria

--- projetoHospital/hospitalDjango/static/scripts/galeria.js
+++ projetoHospital/hospitalDjango/static/scripts/galeria.js
@@ -1,75 +1,125 @@
-var modal = document.querySelector(".modal");
+const $modal = document.querySelector(".modal");
+let images = document.querySelectorAll(".galery-image");
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var images = document.querySelectorAll(".galery-image");
-var modalImg = document.querySelector(".modal-image");
-var modalTitle = document.querySelector(".modal-title");
+const $modalImg = document.querySelector(".modal-image");
+let $modalTitle = document.querySelector(".modal-title");
+const $categories = document.querySelectorAll(".images-category");
 
-for (let i = 0; i < images.length; i++) {
-  images[i].onclick = function () {
-    console.log(i);
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    modalTitle.innerHTML = this.alt;
-  };
-}
-
-// Get the <span> element that closes the modal
-var span = document.querySelectorAll(".close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-  modal.style.display = "none";
-};
-
-var prev = document.querySelectorAll(".modal-previous");
-var next = document.querySelectorAll(".modal-next");
+const $closeBtn = document.querySelector(".close");
+const prev = document.querySelector(".modal-previous");
+const next = document.querySelector(".modal-next");
 
 let imgs = [];
 let alts = [];
-for (let index = 0; index < images.length; index++) {
-  imgs.push(images[index].src);
-  alts.push(images[index].getAttribute("alt"));
+let categs = [];
+
+let allCategs = [];
+
+getContent();
+
+let auxImg;
+let auxAlt;
+let auxCateg;
+
+function getContent() {
+  for (let index = 0; index < $categories.length; index++) {
+    allCategs.push($categories[index].getAttribute("id"));
+  }
+  for (let index = 0; index < images.length; index++) {
+    imgs.push(images[index].src);
+    alts.push(images[index].getAttribute("alt"));
+    categs.push(images[index].getAttribute("categoria"));
+  }
 }
 
-console.log(imgs);
+function displayImage() {
+  for (let i = 0; i < images.length; i++) {
+    images[i].onclick = function () {
+      let list = [];
+      $modal.style.display = "block";
+      $modalImg.src = this.src;
+      $modalImg.setAttribute("categoria", images[i].getAttribute("categoria"));
+      $modalTitle.innerHTML = this.alt;
 
-for (let i = 0; i < prev.length; i++) {
-  let auxImg;
-  let auxAlt;
-  prev[i].addEventListener("click", function () {
-    if (i === 0) {
-      auxImg = imgs[0];
-      auxAlt = alts[0];
-      imgs.shift();
-      alts.shift();
-      imgs.push(auxImg);
-      alts.push(auxAlt);
+      list = imagesByCategory(list, $modalImg.getAttribute("categoria"));
+      selectPrevious(list);
+      selectNext(list);
+    };
+  }
+}
 
-      for (let j = 0; j < imgs.length; j++) {
-        modalImg.src = imgs[j];
-        modalImg.alt = alts[j];
-      }
+function imagesByCategory(list, categoria) {
+  for (let i = 0; i < images.length; i++) {
+    if (categoria == images[i].getAttribute("categoria")) {
+      list = list.concat([[imgs[i], alts[i], categs[i]]]);
     }
-  });
+  }
+  return list;
 }
 
-for (let i = 0; i < next.length; i++) {
-  let auxImg;
-  let auxAlt;
-  next[i].addEventListener("click", function () {
-    if (i === 0) {
-      auxImg = imgs[imgs.length - 1];
-      auxAlt = alts[alts.length - 1];
-      imgs.pop();
-      alts.pop();
-      imgs.unshift(auxImg);
-      alts.unshift(auxAlt);
+$closeBtn.onclick = function () {
+  $modal.style.display = "none";
+};
 
-      for (let j = 0; j < imgs.length; j++) {
-        modalImg.src = imgs[j];
-        modalImg.alt = alts[j];
-      }
-    }
-  });
+function selectPrevious(list) {
+  let imgsCateg = [];
+  let altsCateg = [];
+  let categsCateg = [];
+  for (let i = 0; i < list.length; i++) {
+    imgsCateg.push(list[i][0]);
+    altsCateg.push(list[i][1]);
+    categsCateg.push(list[i][2]);
+  }
+  prev.onclick = function () {
+    auxImg = imgsCateg[0];
+    auxAlt = altsCateg[0];
+    auxCateg = categsCateg[0];
+
+    imgsCateg.shift();
+    altsCateg.shift();
+    categsCateg.shift();
+
+    imgsCateg.push(auxImg);
+    altsCateg.push(auxAlt);
+    categsCateg.push(auxCateg);
+
+    changePosition(imgsCateg, altsCateg, categsCateg);
+  };
 }
+
+function selectNext(list) {
+  let imgsCateg = [];
+  let altsCateg = [];
+  let categsCateg = [];
+  for (let i = 0; i < list.length; i++) {
+    imgsCateg.push(list[i][0]);
+    altsCateg.push(list[i][1]);
+    categsCateg.push(list[i][2]);
+  }
+  next.onclick = function () {
+    auxImg = imgsCateg[imgsCateg.length - 1];
+    auxAlt = altsCateg[altsCateg.length - 1];
+    auxCateg = categsCateg[categsCateg.length - 1];
+
+    imgsCateg.pop();
+    altsCateg.pop();
+    categsCateg.pop();
+
+    imgsCateg.unshift(auxImg);
+    altsCateg.unshift(auxAlt);
+    categsCateg.unshift(auxCateg);
+
+    changePosition(imgsCateg, altsCateg, categsCateg);
+  };
+}
+
+function changePosition(imgsCateg, altsCateg, categsCateg) {
+  for (let j = 0; j < imgsCateg.length; j++) {
+    $modalImg.src = imgsCateg[j];
+    $modalImg.alt = altsCateg[j];
+    $modalImg.setAttribute("categoria", categsCateg[j]);
+    $modalTitle.innerHTML = altsCateg[j];
+  }
+}
+
+displayImage();
