babfafbf9374eff6c9d1c91ef05c7dddc3078763
Larissa-Nahan <larissanahan@gmail.com>
2022-06-22 15:50:16 -0300

    Doação pag

--- projetoHospital/hospitalDjango/static/scripts/doacao.js
+++ projetoHospital/hospitalDjango/static/scripts/doacao.js
@@ -1,19 +1,20 @@
-const $wrapper = document.querySelectorAll(".donate-wrapper");
 const $images = document.querySelectorAll(".donate-image");
-const $infos = document.querySelectorAll(".donate-info-wrapper");
+let $modal = document.querySelectorAll(".modal");
+let $closeBtnDontation = document.querySelectorAll(".close");
 
-function openInfos() {
-  for (let index = 0; index < $wrapper.length; index++) {
-    $images[index].addEventListener("mouseover", function () {
-      $infos[index].style.display = "block";
-      $infos[index].style.display = "flex";
-      $images[index].style.display = "none";
-    });
-    $infos[index].addEventListener("mouseout", function () {
-      $infos[index].style.display = "none";
-      $images[index].style.display = "block";
-    });
+function displayInfos() {
+  for (let i = 0; i < $images.length; i++) {
+    $images[i].onclick = function () {
+      $modal[i].style.display = "flex";
+      close(i);
+    };
   }
 }
 
-openInfos();
+function close(i) {
+  $closeBtnDontation[i].onclick = function () {
+    $modal[i].style.display = "none";
+  };
+}
+
+displayInfos();
