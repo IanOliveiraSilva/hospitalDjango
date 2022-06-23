3b70ff4a6fe94647284a7968e4d724d1f0c9f869
Larissa-Nahan <larissanahan@gmail.com>
2022-06-23 15:39:01 -0300

    Renomeando modal galeria+doacao

--- projetoHospital/hospitalDjango/static/scripts/doacao.js
+++ projetoHospital/hospitalDjango/static/scripts/doacao.js
@@ -1,12 +1,12 @@
 const $images = document.querySelectorAll(".donate-image");
-const $modal = document.querySelectorAll(".modal");
+const $modalDoacao = document.querySelectorAll(".modal");
 let $closeBtnDontation = document.querySelectorAll(".close");
 
 function displayInfos() {
   for (let i = 0; i < $images.length; i++) {
     $images[i].onclick = function () {
       console.log("a");
-      $modal[i].style.display = "flex";
+      $modalDoacao[i].style.display = "flex";
       close(i);
     };
   }
@@ -14,7 +14,7 @@ function displayInfos() {
 
 function close(i) {
   $closeBtnDontation[i].onclick = function () {
-    $modal[i].style.display = "none";
+    $modalDoacao[i].style.display = "none";
   };
 }
 
