3870c72637d43384e23a723553c407e6d5cf965c
Larissa-Nahan <larissanahan@gmail.com>
2022-07-05 16:48:00 -0300

    Pag eventos (periodo bold)

--- projetoHospital/hospitalDjango/static/scripts/eventos.js
+++ projetoHospital/hospitalDjango/static/scripts/eventos.js
@@ -1,6 +1,7 @@
 const eventos = document.querySelectorAll(".event");
 const titles = document.querySelectorAll(".event-title");
 const infos = document.querySelectorAll(".event-info");
+const eventPeriod = document.querySelectorAll(".event-period");
 
 let toggleEvent = [];
 
@@ -13,12 +14,12 @@ for (let i = 0; i < eventos.length; i++) {
     if (toggleEvent[i] == "off") {
       titles[i].style.color = "var(--darkGreen)";
       infos[i].style.whiteSpace = "pre-line";
-      console.log(infos[i].innerHTML);
-      console.log(i);
+      eventPeriod[i].style.fontWeight = "bold";
       toggleEvent[i] = "on";
     } else {
         titles[i].style.color = "black";
         infos[i].style.whiteSpace = "nowrap";
+        eventPeriod[i].style.fontWeight = "400";
         toggleEvent[i] = "off";
     }
   });
